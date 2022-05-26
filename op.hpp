95c17d6ba9aa99d345cbe22800b0b4c83f83b2bf
girumyaye <gyaye001@ucr.edu>
2022-05-25 17:58:34 -0700

    initial opp implementation

---
+++ op.hpp
@@ -0,0 +1,43 @@
+#ifndef __OP_HPP__
+#define __OP_HPP__
+
+#include "base.hpp"
+#include <iostream>
+
+using namespace std;
+
+class Op: public Base {
+    private:
+	double value;
+	Base* value1;
+    public:
+	Op(double value) : Base() {
+	    this -> value = value;
+	}
+
+	~Op() {
+	     delete value1;
+	}
+
+	virtual double evaluate() {
+	    return this -> value;
+	}
+
+	virtual string stringiry() {
+	    string result = to_string(this -> value);
+	    return result;
+	}
+
+	virtual Base* get_child(int i) {
+	    return nullptr;
+	}
+
+	virtual int number_of_children() {
+	    return 0;
+	}
+	//virtual void accept(Visitor* visitor, int index) {
+	//	visitor -> visit_op(this);
+	//	}
+};
+#endif //__OP_HP__
+	
