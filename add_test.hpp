e4ce69d3e8232c7447fc2615ac2627363b2a3f91
girumyaye <gyaye001@ucr.edu>
2022-05-26 09:17:56 -0700

    updated add

--- add_test.hpp
+++ add_test.hpp
@@ -2,10 +2,10 @@
 #define __ADD_TEST_HPP__
 
 #include "gtest/gtest.h"
-
-#include "op.hpp"
-#include "visitor.hpp"
+#include <string>
 #include "add.hpp"
+
+using namespace std;
 
 TEST(AddTest, AddTwoNonZeros) {
     Base* leftVal = new Op (7.0);
@@ -13,8 +13,10 @@ TEST(AddTest, AddTwoNonZeros) {
     Base* test = new Add(leftVal, rightVal);
     EXPECT_DOUBLE_EQ(test->evaluate(), 14.0);
     EXPECT_EQ(test -> stringify(), "(7.000000 + 7.000000)");
-    EXPECT_EQ(test -> get_child(0), nullptr);
-    EXPECT_EQ(test -> number_of_children(), 0);
+    EXPECT_EQ(test -> get_child(0), leftVal);
+    EXPECT_EQ(test-> get_child(1), rightVal);
+    EXPECT_EQ(test-> get_child(2), nullptr);
+    EXPECT_EQ(test -> number_of_children(), 2);
 }
 
 TEST(AddTest, AddPosAndNeg) {
@@ -23,8 +25,22 @@ TEST(AddTest, AddPosAndNeg) {
     Base* test = new Add(leftVal, rightVal);
     EXPECT_DOUBLE_EQ(test->evaluate(), 11.0);
     EXPECT_EQ(test -> stringify(), "(-9.000000 + 20.000000)");
-    EXPECT_EQ(test -> get_child(0), nullptr);
-    EXPECT_EQ(test -> number_of_children(), 0);
+    EXPECT_EQ(test -> get_child(0), leftVal);
+    EXPECT_EQ(test-> get_child(1), rightVal);
+    EXPECT_EQ(test-> get_child(2), nullptr);
+    EXPECT_EQ(test -> number_of_children(), 2);
+}
+
+TEST(AddTest, AddZeroAndNonZero) {
+    Base* leftVal = new Op (0.0);
+    Base* rightVal = new Op (7.0);
+    Base* test = new Add(leftVal, rightVal);
+    EXPECT_DOUBLE_EQ(test->evaluate(), 7.0);
+    EXPECT_EQ(test -> stringify(), "(0.000000 + 7.000000)");
+    EXPECT_EQ(test -> get_child(0), leftVal);
+    EXPECT_EQ(test-> get_child(1), rightVal);
+    EXPECT_EQ(test-> get_child(2), nullptr);
+    EXPECT_EQ(test -> number_of_children(), 2);
 }
 
 #endif //__ADD_TEST_HPP__
