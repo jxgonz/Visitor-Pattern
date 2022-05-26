dac9b42e0b25ff928cd9272ba576bbf1206da60a
jxgonz <johangonzalez7373@gmail.com>
2022-05-25 22:55:17 -0700

    div class implementation and testing

---
+++ div.hpp
@@ -0,0 +1,43 @@
+#ifndef __DIV_HPP__
+#define __DIV_HPP__
+#include <iostream>
+using namespace std;
+
+#include "base.hpp"
+#include "op.hpp"
+
+class Div : public Base {
+    private:
+        Base* left;
+        Base* right;
+
+    public:
+        Div(Base* left, Base* right) : Base() {
+	    this -> left = left;
+	    this -> right = right;
+	}
+
+        double evaluate() override{
+            return this -> left -> evaluate() / this -> right -> evaluate();
+        }
+
+        string stringify() override{
+            return ("(" +  this -> left -> stringify() + " / " + this -> right -> stringify() + ")" );
+        }
+
+	int number_of_children() override {
+	    return 2;
+	}
+
+	Base* get_child(int i) override{
+	    if (i == 0) {
+		return left;
+	    }
+	    if (i == 1) {
+		return right;
+	    }
+	    return nullptr;
+	}
+};
+
+#endif //__DIV_HPP__
